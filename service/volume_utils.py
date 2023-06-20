df161736661ac64ec805c663c9d592d00098317e
Christian R. Garcia <christian.g21@gmail.com>
2023-06-20 15:03:34 -0700

    Changing limit

--- service/volume_utils.py
+++ service/volume_utils.py
@@ -140,7 +140,7 @@ def files_mkdir(system_id: str, path: str = "", tenant_id: str = "", user: str =
     
     logger.info(f"Successfully ran files.mkdir. path: {path}.")
 
-def files_listfiles(system_id: str, path: str, limit: int = 20000, recurse: bool = False, tenant_id: str = "", user: str = "pods") -> List[TapisResult]:
+def files_listfiles(system_id: str, path: str, limit: int = 1000, recurse: bool = False, tenant_id: str = "", user: str = "pods") -> List[TapisResult]:
     """ list in nfs vol
 
     Args:
